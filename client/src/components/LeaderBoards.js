85c44e3eaac1cfe52b3eef09ff0a29d3e8b1fe7d
Laila <laila.mirza@live.co.uk>
2020-08-06 17:29:33 +0100

    fixed import bugs

--- client/src/components/LeaderBoards.js
+++ client/src/components/LeaderBoards.js
@@ -1,11 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import '../App.css';
 import axios from 'axios';
-
-import React, {useState, useEffect} from 'react';
-
-import axios from 'axios';
-import {DataFetch} from './components'
+import DataFetch from './DataFetch'
 
 function Leaderboard(){
     return(
